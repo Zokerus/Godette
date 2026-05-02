f3097688ea509922edea5e97676455ba6d006c66
Zokerus <Zokerus@users.noreply.github.com>
2026-05-02 20:57:34 +0200

    prevent attack spamming

--- scripts/player/character_rig.gd
+++ scripts/player/character_rig.gd
@@ -5,6 +5,7 @@ signal jumpTakeoffRequested
 
 var shieldBlend := 0.0
 var blockLegsBlend := 0.0
+var is_attacking: bool = false
 
 @onready var animation_tree: AnimationTree = $AnimationTree
 @onready var playback: AnimationNodeStateMachinePlayback = animation_tree["parameters/MovementStateMachine/playback"]
@@ -17,7 +18,11 @@ func requestJumpTakeoff() -> void:
 	jumpTakeoffRequested.emit()
 
 func attack()-> void:
-	animation_tree.set("parameters/AttackOneShot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
+	if !is_attacking:
+		animation_tree.set("parameters/AttackOneShot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
+
+func _updateAttackState(value: bool)-> void:
+	is_attacking = value
 
 func defend(delta: float, is_defending: bool, speedRatio: float)-> void:
 	shieldBlend = move_toward(shieldBlend, float(is_defending), delta * 4.0)
