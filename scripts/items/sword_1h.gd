b17e9ec28f76837cc5ad54294e66a66e5e26773f
Zokerus <Zokerus@users.noreply.github.com>
2026-08-02 14:08:39 +0200

    fix bug with already overlapping body and area3D

--- scripts/items/sword_1h.gd
+++ scripts/items/sword_1h.gd
@@ -1,28 +1,53 @@
 class_name Sword1H
 extends Node3D
 
+var hit_bodies: Array[CharacterBody3D] = []
+
 @onready var hit_box: Area3D = $HitBox
 @onready var collision_shape_3d: CollisionShape3D = $HitBox/CollisionShape3D
 
 
 ## Enables the weapon hitbox during the active attack window.
 func enable_hitbox() -> void:
-	collision_shape_3d.disabled = false
-	hit_box.monitoring = true
+	hit_bodies.clear()
+	collision_shape_3d.set_deferred("disabled", false)
+	hit_box.set_deferred("monitoring", true)
+	
+	#wait for the next physics frame
+	await get_tree().physics_frame
+
+	if not hit_box.monitoring:
+		return
+
+	for body in hit_box.get_overlapping_bodies():
+		_process_hit(body)
 
 
 ## Disables the weapon hitbox outside the active attack window.
 func disable_hitbox() -> void:
-	hit_box.monitoring = false
-	collision_shape_3d.disabled = true
+	hit_box.set_deferred("monitoring", false)
+	collision_shape_3d.set_deferred("disabled", true)
+
+
+func _process_hit(body: CharacterBody3D):
+		#body got damage on this try, deal damage just once
+		if body in hit_bodies:
+			return
+		
+		hit_bodies.append(body)
+		
+		var combat_component := body.get_node_or_null("CombatComponent") as CombatComponent
+		if combat_component == null:
+			return
+			
+		combat_component.getHit(&"LightHit")
+		
+		#TODO set HIT Animation name
+		body.combat_component.getHit("test")
 
 
 func _on_hit_box_body_entered(body: Node3D) -> void:
-	print("Weapon touched: ", body.name)
-	if body is Enemy:
-		#TODO set HIT Animation name
-		body.combat_component.getHit("test")
-	
+		_process_hit(body)
 
 
 #func _on_animation_event_relay_component_animation_event_received(event: AnimationEventRelay.AnimationEvents) -> void:
