061de9dc5d97ec9159bbd01d8bfd942060c3d020
Zokerus <Zokerus@users.noreply.github.com>
2026-08-14 14:17:23 +0200

    add projectile class und add damage to fireball

--- scripts/projectiles/fireball.gd
+++ scripts/projectiles/fireball.gd
@@ -1,9 +1,12 @@
 class_name Fireball
-extends Area3D
+extends Projectile
 
 var direction: Vector3
 
 const SPEED = 5.0 # temp moving speed, might change in the future
+
+func _ready() -> void:
+	basic_damage = 10
 
 func initialize(dir: Vector3)-> void:
 	direction = dir
@@ -11,3 +14,12 @@ func initialize(dir: Vector3)-> void:
 
 func _physics_process(delta: float) -> void:
 	position += direction.normalized() * SPEED *delta
+
+
+func _on_body_entered(body: Node3D) -> void:
+	var combat_component := body.get_node_or_null("CombatComponent") as CombatComponent
+	if combat_component == null:
+		return
+	
+	combat_component.getHit(&"LightHit", get_damage())
+	queue_free()
---
+++ scripts/projectiles/projectile.gd
@@ -0,0 +1,10 @@
+class_name Projectile
+extends Area3D
+
+#TODO type of damage and damage over time
+
+var basic_damage: float = 10.0 # might be set by weapon_attributes in the future
+
+## return damage of the weapon, defense and buffs of the target are not included
+func get_damage()-> float:
+	return basic_damage
